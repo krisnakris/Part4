2586ff85dd07cee21cb48598bc5546d9c1aad1e3
krisnakris <krisna.post@gmail.com>
2021-05-03 17:13:53 +0700

    add async error extension, part 4.8-4.12 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,7 @@
 const Blog = require('../models/blog');
 
 class BlogController {
-  static async getAllBlogs(req, res, next) {
+  static async getAllBlogs(req, res) {
     // Blog
     //   .find({})
     //   .then((blogs) => {
@@ -10,29 +10,41 @@ class BlogController {
     //   .catch((err) => {
     //     next(err);
     //   });
-    try {
-      const blogs = await Blog.find({});
-      res.json(blogs);
-    } catch (error) {
-      next(error);
+
+    // try {
+    //   const blogs = await Blog.find({});
+    //   res.json(blogs);
+    // } catch (error) {
+    //   next(error);
+    // }
+
+    const blogs = await Blog.find({});
+    res.json(blogs);
+  }
+
+  static async getSingleBlogs(req, res) {
+    // try {
+    //   const blog = await Blog.findById(req.params.id);
+
+    //   if (blog) {
+    //     res.json(blog);
+    //   } else {
+    //     res.status(404).end();
+    //   }
+    // } catch (error) {
+    //   next(error);
+    // }
+
+    const blog = await Blog.findById(req.params.id);
+
+    if (blog) {
+      res.json(blog);
+    } else {
+      res.status(404).end();
     }
   }
 
-  static async getSingleBlogs(req, res, next) {
-    try {
-      const blog = await Blog.findById(req.params.id);
-
-      if (blog) {
-        res.json(blog);
-      } else {
-        res.status(404).end();
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  static async postBlog(req, res, next) {
+  static async postBlog(req, res) {
     // const blog = new Blog(req.body);
     // blog
     //   .save()
@@ -43,39 +55,55 @@ class BlogController {
     //     next(error);
     //   });
 
-    try {
-      const blog = await new Blog(req.body).save();
+    // try {
+    //   const blog = await new Blog(req.body).save();
 
-      res.status(201).json(blog);
-    } catch (error) {
-      next(error);
-    }
+    //   res.status(201).json(blog);
+    // } catch (error) {
+    //   next(error);
+    // }
+
+    const blog = await new Blog(req.body).save();
+
+    res.status(201).json(blog);
   }
 
-  static async deleteBlogs(req, res, next) {
-    try {
-      await Blog.findByIdAndDelete(req.params.id);
-      res.status(204).end();
-    } catch (error) {
-      next(error);
-    }
+  static async deleteBlogs(req, res) {
+    // try {
+    //   await Blog.findByIdAndDelete(req.params.id);
+    //   res.status(204).end();
+    // } catch (error) {
+    //   next(error);
+    // }
+
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).end();
   }
 
-  static async updateBlogs(req, res, next) {
-    try {
-      const { body } = req;
-      const newBlog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-      };
+  static async updateBlogs(req, res) {
+    // try {
+    //   const { body } = req;
+    //   const newBlog = {
+    //     title: body.title,
+    //     author: body.author,
+    //     url: body.url,
+    //   };
 
-      const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, newBlog);
-      console.log('updatedBlog: ', updatedBlog);
-      res.json(updatedBlog);
-    } catch (error) {
-      next(error);
-    }
+    //   const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, newBlog);
+    //   res.json(updatedBlog);
+    // } catch (error) {
+    //   next(error);
+    // }
+
+    const { body } = req;
+    const newBlog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+    };
+
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, newBlog);
+    res.json(updatedBlog);
   }
 }
 
